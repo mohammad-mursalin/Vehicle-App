206722dee739f2f89a68d13cd343f161244edc95
mohammad_mursalin <mursalinlamon@gmail.com>
2024-03-02 22:02:23 +0600

    vehicle class

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        SUV fortuner = new SUV("Fortuner", false);
+
+        fortuner.move(30, 0);
+        fortuner.accelarate(80);
+
+    }
+}
